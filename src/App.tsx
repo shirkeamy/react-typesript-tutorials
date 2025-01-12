6eb5e0333efbbf2921eb40f17f6f6e723918b57f
Amol Shirke <shirkeamy@gmail.com>
2025-01-12 21:31:02 +0530

    Add routing into application

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,20 @@
 import React from 'react';
 import './App.css';
-import Menu from './Components/Menu';
-import NotFoundErrorPage from './Components/ErrorPage';
-import CompanyMaster from './Components/CompanyMaster';
 import ItemMaster from './Components/ItemMaster';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './Components/Layout';
+import CompanyMaster from './Components/CompanyMaster';
 
 function App() {
 
-  let userName: string = "John Doe";
-
-  let menuList: string[] = ["Master", "Admin", "Home"];
-
   return (
-    <div className="App">
-      <h1>Welcome to react, {userName} </h1>
-      <img src="" alt="" />
-      <p>
-        {
-          5 + 5
-        }
-      </p>
-
-      {/* <Menu /> */}
-      {/* <hr />
-      <h1>NotFoundErrorPage Component</h1> */}
-      {/* <NotFoundErrorPage></NotFoundErrorPage> */}
-      {/* <NotFoundErrorPage /> */}
-      {/* <hr />
-      <h1>Company master component</h1>
-      <CompanyMaster />
-      <hr />
-      <h1>Item master component</h1> */}
-      <ItemMaster itemList={["Item1", "Item2", "Item3", "Item4"]} ></ItemMaster>
-
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="item-master" element={<ItemMaster itemList={["Item1", "Item2", "Item3", "Item4"]} />} />
+          <Route path="company-master" element={<CompanyMaster />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css"
+
+const Layout: React.FC = () =>{
+
+    return (
+        <ul>
+            <li><a href="item-master">Item Master</a></li>
+            <li><a href="company-master">Company Master</a></li>
+        </ul>
+    )
+
+}
+
+export default Layout;
